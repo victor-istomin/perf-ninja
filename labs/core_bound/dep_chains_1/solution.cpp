0af46235fd29768bdae71d52cd22cacd1c2f9ded
Victor Istomin <victor.send.mail.here@gmail.com>
2022-10-31 13:13:10 +0000

    solved: dep_chains_1

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -1,5 +1,6 @@
 #include "solution.hpp"
 #include <array>
+#include <algorithm>
 #include <iostream>
 
 unsigned getSumOfDigits(unsigned n) {
@@ -20,22 +21,90 @@ unsigned getSumOfDigits(unsigned n) {
 // Hint: Traversing a linked list is a long data dependency chain:
 //       to get the node N+1 you need to retrieve the node N first.
 //       Think how you can execute multiple dependency chains in parallel.
+template <typename ValueType, size_t MaxSize>
+class Bucket
+{
+public:
+
+  using Buffer   = std::array<ValueType, MaxSize>;
+  using iterator = typename Buffer::iterator;
+
+private:
+
+  Buffer   m_buffer;
+  iterator m_tail;
+
+public:
+
+  Bucket()  { m_tail = m_buffer.begin(); }
+
+  void push(ValueType v) 
+  {
+    *m_tail = v;
+    ++m_tail;
+  }
+
+  void erase(iterator where)
+  {
+    auto next = std::next(where);
+    std::copy(next, m_tail, where);
+    //--m_tail;
+    //*where = *m_tail;       // swap deleted element with last one
+
+  }
+
+  iterator find(ValueType v) 
+  { 
+      auto itFound = std::lower_bound(m_buffer.begin(), m_tail, v);
+      return *itFound == v ? itFound : m_tail; 
+  }
+
+  iterator begin() const     { return m_buffer.begin(); }
+  iterator begin()           { return m_buffer.begin(); }
+  iterator end() const       { return m_tail; }
+  iterator end()             { return m_tail; }
+
+  bool isEmpty() const { return m_tail == m_buffer.begin(); }
+  bool isFull() const  { return m_tail == m_buffer.end(); }
+};
+
+
 unsigned solution(List *l1, List *l2) {
   unsigned retVal = 0;
 
+  constexpr size_t BucketSize = 8 * 1024;
+
   List *head2 = l2;
+
   // O(N^2) algorithm:
-  while (l1) {
-    unsigned v = l1->value;
+  while (l1) 
+  {
+    Bucket<unsigned, BucketSize> bucket;
+    while (!bucket.isFull() && l1)
+    {
+      bucket.push(l1->value);
+      l1 = l1->next;
+    }
+
+    std::sort(bucket.begin(), bucket.end());
+
     l2 = head2;
-    while (l2) {
-      if (l2->value == v) {
+    while (l2) 
+    {
+      unsigned v = l2->value;
+      auto itFound = bucket.find(v);
+      if (itFound != bucket.end())
+      {
         retVal += getSumOfDigits(v);
-        break;
+        bucket.erase(itFound);
+        //std::sort(bucket.begin(), bucket.end());
+
+        if (bucket.isEmpty())
+          break;
       }
+
       l2 = l2->next;
     }
-    l1 = l1->next;
   }
 
   return retVal;
