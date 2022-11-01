a4ae8573c91e3a486f8429a41634740a33e5a637
Victor Istomin <victor.send.mail.here@gmail.com>
2022-11-01 13:55:25 +0200

    virtual_call_mispredict: done, let the calls be ordered

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -1,26 +1,35 @@
 #include "solution.h"
 
 #include <random>
+#include <typeindex>
 
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
+    InstanceArray arrays[3];
+
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
         if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
+          arrays[value].push_back(std::make_unique<ClassA>());
         } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
+          arrays[value].push_back(std::make_unique<ClassB>());
         } else {
-            array.push_back(std::make_unique<ClassC>());
+          arrays[value].push_back(std::make_unique<ClassC>());
         }
     }
+
+    array.reserve(arrays[0].size() + arrays[1].size() + arrays[2].size());
+    for(InstanceArray& a : arrays)
+      std::move(a.begin(), a.end(), std::back_inserter(array));
 }
 
 // Invoke the `handle` method on all instances in `output`
-void invoke(InstanceArray& array, std::size_t& data) {
-    for (const auto& item: array) {
+void invoke(InstanceArray& array, std::size_t& data) 
+{
+    for (const auto& item: array) 
+    {
         item->handle(data);
     }
 }
